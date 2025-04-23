1c567ae8d315c87cecfff0835152349071f8f98c
atzama <atzama.uth.gr>
2025-04-23 19:58:36 +0300

    upload: askisi1.pl

---
+++ askisi1.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+# Δήλωση ακολουθίας DNA
+my $dna = "ATGCTAGC";
+
+# Εμφάνιση μήκους
+my $length = length($dna);
+print "Length of sequence: $length\n";
+
+# Μετατροπή σε πεζά
+my $lower = lc($dna);
+print "Lowercase sequence: $lower\n";
+
+# Αντιστροφή ακολουθίας
+my $reversed = reverse($dna);
+print "Reversed sequence: $reversed\n";
+
+s
